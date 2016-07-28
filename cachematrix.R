d50784f927e03885a26219e0b27445903448bddb
Christian Bartl <x-github@cbartl.com>
2016-07-28 02:11:00 +0200

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These functions are used to calculate inverse of a square matrix. The result is cached when
+# it gets calculated.
+# Usage:
+#   - Create a cache object for matrix using 
+#     mycache <- makeCacheMatrix( mymatrix )
+#   - Get the inverserse with
+#     myinverse <- cacheSolve( mycache )
+# Example:
+#   > m2x2 <- matrix( 1:4, 2, 2 )
+#   > cache2x2 <- makeCacheMatrix( m2x2 )
+#   > cacheSolve( cache2x2 )
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+# Creates a cache matrix object which is used by cacheSolve().
+# Param x ... A square matrix that will be inversed by cacheSolve()
+# Returns a list with internal representation of the object.
+makeCacheMatrix <- function( x = matrix() ) {
+    inversedX <- NULL
+    set <- function( y ) {
+        x <<- y
+        inversedX <<- NULL
+    }
+    get <- function() x
+    setInverse <- function( inv )  {
+        inversedX <<- inv
+    }
+    getInverse <- function() {
+        inversedX
+    }
+    list( set = set, get = get,
+          setInverse = setInverse,
+          getInverse = getInverse )
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Get the inverse of a cache matrix object.
+# Param x ... A cache matrix object created by makeCacheMatrix()
+# Returns a matrix that is the inverse of 'x'.
+cacheSolve <- function( x, ... ) {
+    inversedData <- x$getInverse()
+    if( !is.null( inversedData ) ) {
+        message( "using cached data" )
+        return( inversedData )
+    }
+    message( "not in cache, calculating inversion" )
+    data <- x$get()
+    inversedData <- solve( data )#, ... )
+    x$setInverse( inversedData )
+    inversedData
 }
